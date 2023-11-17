1758136c05459f5b2eb2d5a5e3d2eec285155b6e
jugiros <jugiros1@gmail.com>
2023-11-17 17:45:09 -0500

    feat: Estructura inicial para el microervicio Cuenta-Movimientos.

---
+++ Cuenta-Movimientos/src/main/java/com/challenge/CuentaMovimientos/CuentaMovimientosApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.CuentaMovimientos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuentaMovimientosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuentaMovimientosApplication.class, args);
+	}
+
+}
---
+++ Cuenta-Movimientos/src/test/java/com/challenge/CuentaMovimientos/CuentaMovimientosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.CuentaMovimientos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuentaMovimientosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
