654dd6d111e806878b90bdde493a87c5142e4c02
jugiros <jugiros1@gmail.com>
2023-11-18 19:07:14 -0500

    docs: Actualización del archivo para la creación de la base de datos.

--- BaseDatos.sql
+++ BaseDatos.sql
@@ -1,44 +1,75 @@
--- Tabla Persona
-CREATE TABLE IF NOT EXISTS persona (
-                                       id BIGINT NOT NULL AUTO_INCREMENT,
-                                       nombre VARCHAR(255),
-    genero VARCHAR(50),
-    edad INT,
-    identificacion VARCHAR(100),
-    direccion VARCHAR(255),
-    telefono VARCHAR(50),
-    PRIMARY KEY (id)
-    );
+/*
+SQLyog Community v13.2.1 (64 bit)
+MySQL - 8.2.0 : Database - devsu_challenge
+*********************************************************************
+*/
 
--- Tabla Cliente (que hereda de Persona)
-CREATE TABLE IF NOT EXISTS cliente (
-                                       id BIGINT NOT NULL,
-                                       clienteId BIGINT NOT NULL AUTO_INCREMENT,
-                                       contraseña VARCHAR(255),
-    estado VARCHAR(50),
-    PRIMARY KEY (clienteId),
-    FOREIGN KEY (id) REFERENCES persona(id)
-    );
+/*!40101 SET NAMES utf8 */;
 
--- Tabla Cuenta
-CREATE TABLE IF NOT EXISTS cuenta (
-                                      id BIGINT NOT NULL AUTO_INCREMENT,
-                                      numero_cuenta VARCHAR(100),
-    tipo_cuenta VARCHAR(50),
-    saldo_inicial DECIMAL(10, 2),
-    estado VARCHAR(50),
-    PRIMARY KEY (id)
-    );
+/*!40101 SET SQL_MODE=''*/;
 
--- Tabla Movimientos
-CREATE TABLE IF NOT EXISTS movimientos (
-                                           id BIGINT NOT NULL AUTO_INCREMENT,
-                                           fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                                           tipo_movimiento VARCHAR(50),
-    valor DECIMAL(10, 2),
-    saldo DECIMAL(10, 2),
-    PRIMARY KEY (id)
-    );
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`devsu_challenge` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
-ALTER TABLE `cliente`
-    CHANGE `estado` `estado` BOOLEAN NULL;
+USE `devsu_challenge`;
+
+/*Table structure for table `cliente` */
+
+DROP TABLE IF EXISTS `cliente`;
+
+CREATE TABLE `cliente` (
+  `clienteId` bigint NOT NULL AUTO_INCREMENT,
+  `contraseña` varchar(255) DEFAULT NULL,
+  `estado` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`clienteId`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Table structure for table `cuenta` */
+
+DROP TABLE IF EXISTS `cuenta`;
+
+CREATE TABLE `cuenta` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `numero_cuenta` varchar(100) DEFAULT NULL,
+  `tipo_cuenta` varchar(50) DEFAULT NULL,
+  `saldo_inicial` decimal(10,2) DEFAULT NULL,
+  `estado` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Table structure for table `movimientos` */
+
+DROP TABLE IF EXISTS `movimientos`;
+
+CREATE TABLE `movimientos` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `fecha` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `tipo_movimiento` varchar(50) DEFAULT NULL,
+  `valor` decimal(10,2) DEFAULT NULL,
+  `saldo` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Table structure for table `persona` */
+
+DROP TABLE IF EXISTS `persona`;
+
+CREATE TABLE `persona` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) DEFAULT NULL,
+  `genero` varchar(50) DEFAULT NULL,
+  `edad` int DEFAULT NULL,
+  `identificacion` varchar(100) DEFAULT NULL,
+  `direccion` varchar(255) DEFAULT NULL,
+  `telefono` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_identificacion` (`identificacion`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
