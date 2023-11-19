dc586061a52fce55465766878540c59034112a99
jugiros <jugiros1@gmail.com>
2023-11-18 20:07:02 -0500

    feat: Integración de pruebas unitarias para la creación y actualización de un cliente.

--- Cliente-Persona/src/test/java/com/challenge/ClientePersona/ClientePersonaApplicationTests.java
+++ Cliente-Persona/src/test/java/com/challenge/ClientePersona/ClientePersonaApplicationTests.java
@@ -1,13 +1,58 @@
 package com.challenge.ClientePersona;
 
+import com.challenge.ClientePersona.controllers.ClienteController;
+import com.challenge.ClientePersona.dto.PersonaClienteDTO;
+import com.challenge.ClientePersona.models.Cliente;
+import com.challenge.ClientePersona.services.ClienteService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ClientePersonaApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Mock
+	private ClienteService clienteService;
+
+	@InjectMocks
+	private ClienteController clienteController;
+
+	@BeforeEach
+	public void setUp() {
+		MockitoAnnotations.openMocks(this);
 	}
 
+	@Test
+	public void testCreateClienteFromDTO() {
+		PersonaClienteDTO personaClienteDTO = new PersonaClienteDTO();
+
+		Cliente clienteMock = new Cliente();
+		when(clienteService.createClienteFromDTO(any(PersonaClienteDTO.class))).thenReturn(clienteMock);
+
+		ResponseEntity<Cliente> response = clienteController.createClienteFromDTO(personaClienteDTO);
+
+		assertEquals(HttpStatus.CREATED, response.getStatusCode());
+	}
+
+	@Test
+	public void testUpdatePersonaAndClienteFromClienteId() {
+		Long id = 1L;
+		PersonaClienteDTO personaClienteDTO = new PersonaClienteDTO();
+
+		Cliente clienteMock = new Cliente();
+
+		when(clienteService.updatePersonaAndClienteFromClienteId(eq(id), any(PersonaClienteDTO.class))).thenReturn(clienteMock);
+
+		ResponseEntity<Cliente> response = clienteController.updatePersonaAndClienteFromClienteId(id, personaClienteDTO);
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+	}
 }
