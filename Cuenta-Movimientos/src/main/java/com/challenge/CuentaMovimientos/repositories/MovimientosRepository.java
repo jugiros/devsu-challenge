f0e31179be854450141a04f3440e8b18660bde23
jugiros <jugiros1@gmail.com>
2023-11-20 12:49:20 -0500

    feat: Creación de query para obtener el último movimiento generado.

--- Cuenta-Movimientos/src/main/java/com/challenge/CuentaMovimientos/repositories/MovimientosRepository.java
+++ Cuenta-Movimientos/src/main/java/com/challenge/CuentaMovimientos/repositories/MovimientosRepository.java
@@ -1,9 +1,13 @@
 package com.challenge.CuentaMovimientos.repositories;
 import com.challenge.CuentaMovimientos.models.Movimientos;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MovimientosRepository extends JpaRepository<Movimientos, Long> {
+
+    @Query("SELECT m FROM Movimientos m ORDER BY m.fecha DESC")
+    Movimientos findUltimoMovimiento();
 
 }
