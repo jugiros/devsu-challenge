6d7f76fd63c94478885bbc8744801624b1fb86a2
jugiros <jugiros1@gmail.com>
2023-11-20 12:15:11 -0500

    feat: Actualización para el control de excepciones.

---
+++ Cuenta-Movimientos/src/main/java/com/challenge/CuentaMovimientos/exceptions/CuentaNotFoundException.java
@@ -0,0 +1,7 @@
+package com.challenge.CuentaMovimientos.exceptions;
+
+public class CuentaNotFoundException extends RuntimeException {
+
+    public CuentaNotFoundException(String message) { super(message); }
+
+}
---
+++ Cuenta-Movimientos/src/main/java/com/challenge/CuentaMovimientos/exceptions/CustomExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.challenge.CuentaMovimientos.exceptions;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(CuentaNotFoundException.class)
+    public ResponseEntity<Object> handleCuentaNotFoundException(CuentaNotFoundException ex) {
+        String message = ex.getMessage();
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(message);
+    }
+
+}
